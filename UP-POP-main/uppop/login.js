531ad3b32ccce2cb97b0f833e6e00d314f0ea7d1
poliao <119989213+poliao@users.noreply.github.com>
2023-09-03 20:17:26 +0700

    ห
    ห

--- UP-POP-main/uppop/login.js
+++ UP-POP-main/uppop/login.js
@@ -1,27 +1,3 @@
-// login.js
-
-const mysql = require('mysql');
-
-const dbConfig = {
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'up pop'
-};
-
-// สร้างการเชื่อมต่อกับ MySQL
-const connection = mysql.createConnection(dbConfig);
-
-// เรียกใช้การเชื่อมต่อ
-connection.connect((err) => {
-    if (err) {
-        console.error('เกิดข้อผิดพลาดในการเชื่อมต่อกับ MySQL:', err);
-    } else {
-        console.log('เชื่อมต่อกับ MySQL สำเร็จ');
-        // คุณสามารถเรียกใช้คำสั่ง SQL และดำเนินการกับฐานข้อมูลได้ที่นี่
-    }
-});
-
 // ฟังก์ชันเพื่อตรวจสอบข้อมูลการเข้าสู่ระบบ
 function performLogin() {
     // ตรวจสอบความถูกต้องของข้อมูลที่รับมา (นี่คือตัวอย่างแบบเบื้องต้น)
@@ -33,21 +9,24 @@ function performLogin() {
         return;
     }
 
-    // สร้างคำสั่ง SQL สำหรับค้นหาผู้ใช้ในฐานข้อมูล
-    const sqlQuery = `SELECT * FROM user WHERE username = ? AND password = ?`;
-
-    // ทำการค้นหาในฐานข้อมูล
-    connection.query(sqlQuery, [username, password], (err, results) => {
-        if (err) {
-            console.error('เกิดข้อผิดพลาดในการค้นหาในฐานข้อมูล:', err);
-            return;
-        }
-
-        if (results.length > 0) {
+    // ทำการค้นหาในฐานข้อมูลผ่านการร้องขอไปยังเซิร์ฟเวอร์
+    fetch('/api/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ username, password })
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.success) {
             alert("เข้าสู่ระบบสำเร็จ");
             // ทำอะไรก็ตามหลังจากเข้าสู่ระบบสำเร็จ เช่น นำผู้ใช้ไปยังหน้าอื่น ๆ
         } else {
             alert("ชื่อผู้ใช้หรือรหัสผ่านไม่ถูกต้อง");
         }
+    })
+    .catch(error => {
+        console.error('เกิดข้อผิดพลาดในการร้องขอไปยังเซิร์ฟเวอร์:', error);
     });
 }
